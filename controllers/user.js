6a600b59a950b2a4093ba07934f3ffa95aa1b745
vitalikzahar <vitalikzahar29@gmail.com>
2023-11-27 19:05:59 +0200

    end

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,20 @@
 const fs = require("node:fs/promises");
 const path = require("node:path");
-// const Jimp = require("jimp");
+const Jimp = require("jimp");
 const { User } = require("../models/user");
 
 async function uploadAvatar(req, res, next) {
   try {
+    const { path: tempUpload} = req.file;
+
+    Jimp.read(tempUpload, (err, image) => {
+      if (err) throw err;
+      image
+        .resize(200, 200) 
+
+        .write(tempUpload); 
+    });
+
     await fs.rename(
       req.file.path,
       path.join(__dirname, "..", "public/avatars", req.file.filename)
