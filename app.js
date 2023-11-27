829d409815bd610f7bd624ca72c2fa7a436d5fce
vitalikzahar <vitalikzahar29@gmail.com>
2023-11-27 15:33:31 +0200

    confirm

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require("./db");
+
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
 const auth = require("./middleware/auth");
-
+const path = require("node:path");
 const contactsRouter = require("./routes/api/contacts");
 
 const app = express();
@@ -15,6 +16,8 @@ const authRoutes = require("./routes/api/auth");
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
+
+app.use("/avatars", express.static(path.join(__dirname, "public", "avatars")));
 
 app.use("/api/users", authRoutes);
 
